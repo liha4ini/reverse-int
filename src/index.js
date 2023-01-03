3f8c22a7bad29a4624f3cf979dee443944fb1941
Sergey Lihachev <liha4ev.sergey@gmail.com>
2023-01-03 13:52:38 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+
+    if (n < 0) {
+        return Math.abs(n).toString().split("").reverse().join("");
+    } else {
+        return n.toString().split("").reverse().join("");
+    }
 }
